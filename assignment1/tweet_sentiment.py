cf60bd22f91e0f9eb0d8136c424eaa2357a5f9c4
Alan Jackoway <alan.jackoway@gmail.com>
2013-05-06 14:55:52 -0700

    Assignment 1 problem 2

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,4 +1,9 @@
 import sys
+import json
+from collections import defaultdict
+
+# Kind of a stupid way to do this, but matches
+PUNCTUATION_TO_TRIM = ".,?!\"'[]_+-=\()@$%^&*;:"
 
 def hw():
     print 'Hello, world!'
@@ -6,12 +11,37 @@ def hw():
 def lines(fp):
     print str(len(fp.readlines()))
 
+def build_sentiment(sent_fp):
+    result = defaultdict(float)
+    for line in sent_fp:
+        parts = line.decode("utf-8").split("\t")
+        result[parts[0]] = float(parts[1])
+    return result
+
+def handle_tweets(tweet_fp, sentiment):
+    for line in tweet_fp:
+        tweet_json = json.loads(line)
+        if "text" not in tweet_json:
+            # Could be a delete.
+            print "0.0"
+            continue
+        tweet_text = tweet_json["text"]
+        tweet_text.lower()
+        total = 0.0
+        tweet_words = tweet_text.split()
+        # Figure out which terms are in this tweet. Multiple-word phrases from the sentiment file are not handled
+        for w in tweet_words:
+            # Remove trailing punctuation
+            w = w.lstrip(PUNCTUATION_TO_TRIM).rstrip(PUNCTUATION_TO_TRIM)
+            total += sentiment[w]
+        print total
+
 def main():
     sent_file = open(sys.argv[1])
+    sentiment = build_sentiment(sent_file)
+    sent_file.close()
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
-
+    handle_tweets(tweet_file, sentiment)
+    tweet_file.close()
 if __name__ == '__main__':
     main()
